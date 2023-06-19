ff7a238957f13e3e00efc2be39e6d5691c36c9c1
Alexander Pervyshev <kingtiger27062004@gmail.com>
2023-06-19 20:52:19 +0300

    versuche ein Profilbild zu erstellen

--- social_media/profiles/models.py
+++ social_media/profiles/models.py
@@ -1,15 +1,29 @@
 from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
-    profile_pic = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    profile_pic = models.ImageField(default="default.jpg", null=True, blank=True, upload_to="profile_pics")
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     age = models.IntegerField(default=21)
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.profile_pic.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.profile_pic.path)
 
 
 def __str__(self):
