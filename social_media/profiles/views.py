598af226d8c795c494551dcf41b983adb05e2ad0
Alexander Pervyshev <kingtiger27062004@gmail.com>
2023-07-03 17:41:24 +0300

    Post_form (post-create) hat gemacht

--- social_media/profiles/views.py
+++ social_media/profiles/views.py
@@ -155,6 +155,7 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
+    template_name = 'post_confirm_delete.html
     success_url = '/'
 
     def test_func(self):
