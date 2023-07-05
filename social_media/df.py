db8924f19495e919f6e45ea2cae0c51977f86363
Alexander Pervyshev <kingtiger27062004@gmail.com>
2023-07-05 15:25:38 +0300

    Post_form (post-create) hat gemacht

--- social_media/df.py
+++ social_media/df.py
@@ -8,6 +8,26 @@ class Person:
         print(f"{self.name} is {self.age}")
 
 
-Alex = Person("alex")
+class Student(Person):
+    def study(self):
+        print(f"{self.name} is studying")
+
+
+Alex = Student("Alex")
 Alex.age = 20
 Alex.display_info()
+Alex.study()
+
+
+class Phone:
+    def __init__(self, name, color, model):
+        self.name = name
+        self.color = color
+        self.model = model
+
+    def display_attribute(self):
+        print(f"{self.name} has color {self.color} and model {self.model}")
+
+
+Iphone = Phone("IPhone", "black", 14)
+Iphone.display_attribute()
